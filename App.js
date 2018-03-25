eb6d9a25fc2f50d780885132c922021145bac066
st-fresh <jmj06c@my.fsu.edu>
2018-03-24 17:15:58 -0700

    added tokbox

--- App.js
+++ App.js
@@ -1,58 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { View } from 'react-native';
+import { OTSession, OTPublisher, OTSubscriber } from 'opentok-react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.apiKey = '46087002';
+    this.sessionId = '2_MX40NjA4NzAwMn5-MTUyMTkzNDc4ODMxOX5ZTEo0STY4Z2E1Y0JRSGM3S3NORGNSb1d-fg';
+    this.token = 'T1==cGFydG5lcl9pZD00NjA4NzAwMiZzaWc9NzI5MzYxNzRlZWUxZjNiMWUyN2NkMGJlZTVlMzgxNjk2MTlkNGJjYjpzZXNzaW9uX2lkPTJfTVg0ME5qQTROekF3TW41LU1UVXlNVGt6TkRjNE9ETXhPWDVaVEVvMFNUWTRaMkUxWTBKUlNHTTNTM05PUkdOU2IxZC1mZyZjcmVhdGVfdGltZT0xNTIxOTM0ODE3Jm5vbmNlPTAuMjI3NzQ2NDY5NDk3NDM0Mjcmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTUyNDUyNjgxNyZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==';
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View style={{ flex: 1, flexDirection: 'row' }}>
+        <OTSession apiKey={this.apiKey} sessionId={this.sessionId} token={this.token}>
+          <OTPublisher style={{ width: 100, height: 100 }} />
+          <OTSubscriber style={{ width: 100, height: 100 }} />
+        </OTSession>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
